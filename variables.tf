296987917d000bda67c6ddc6e4a3725f1cc18eff
Rania <raniayaqoubi1995@gmail.com>
2024-02-21 16:03:52 +0100

    re-organising terraform files

--- variables.tf
+++ variables.tf
@@ -10,14 +10,8 @@ variable "app_name" {
   default     = "pyspark-app"
 }
 
-variable "ami" {
-  description = "Amazon machine image"
-  type        = string
-  default     = "ami-01b32e912c60acdfa" # Ubuntu 22.04 LTS 
-}
-
-variable "instance_type" {
-  description = "ec2 instance type"
-  type        = string
-  default     = "t2.micro"
+variable "bucket_prefix" {
+  description = "S3 bucket prefix"
+  type = string
+  default = "gl-pyspark"
 }
